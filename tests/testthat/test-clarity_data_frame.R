0ac96499616489028da8590a726f5c18f1ce59f2
Nick Pelikan <nick.pelikan@yougov.com>
2018-01-23 18:49:55 -0800

    some erroring tests

--- tests/testthat/test-clarity_data_frame.R
+++ tests/testthat/test-clarity_data_frame.R
@@ -1,5 +1,16 @@
 context("test-clarity_data_frame.R")
 
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+## failing test -- 2018-01-18
+test_that("GA Format works", {
+  expect_is(as.data.frame(clarity_get("http://results.enr.clarityelections.com/GA/Appling/63993/en/reports.html")), "data.frame")
 })
+
+## fails
+test_that("NJ/Old format works", {
+    expect_is(as.data.frame(clarity_get("http://results.enr.clarityelections.com/NJ/Cape_May/71890/190686/Web01/en/summary.html")), "data.frame")
+})
+
+test_that("Madison/New format works", {
+    expect_is(as.data.frame(clarity_get("http://results.enr.clarityelections.com/AL/Madison/72211/Web02/#/")), "data.frame")
+})
+
