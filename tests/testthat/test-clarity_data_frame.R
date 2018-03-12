9851788914ac27d071c28c2a8b3d8148d0a26468
Nick Pelikan <nick.pelikan@gmail.com>
2018-03-12 12:54:43 -0700

    Update test-clarity_data_frame.R

--- tests/testthat/test-clarity_data_frame.R
+++ tests/testthat/test-clarity_data_frame.R
@@ -1,11 +1,9 @@
 context("test-clarity_data_frame.R")
 
-## failing test -- 2018-01-18
 test_that("GA Format works", {
   expect_is(as.data.frame(clarity_get("http://results.enr.clarityelections.com/GA/Appling/63993/en/reports.html")), "data.frame")
 })
 
-## fails
 test_that("NJ/Old format works", {
     expect_is(as.data.frame(clarity_get("http://results.enr.clarityelections.com/NJ/Cape_May/71890/190686/Web01/en/summary.html")), "data.frame")
 })
